58a1e26fb71b571b7cfdf0ad10d4e0ef22263758
Nick Bennett <nick@nickbenn.com>
2020-10-30 10:04:36 -0600

    Persistent data model in progress; added enum, etc.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Guess.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/model/entity/Guess.java
@@ -31,7 +31,7 @@ public class Guess {
   @ColumnInfo(name = "game_id", index = true)
   private long gameId;
 
-  @ColumnInfo(name = "guess_key")
+  @ColumnInfo(name = "guess_key", typeAffinity = ColumnInfo.BLOB)
   private UUID guessKey;
 
   @NonNull
