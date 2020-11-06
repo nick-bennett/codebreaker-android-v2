af973b80771b52b5a3819ee80b2534c8677c2a1f
Nick Bennett <nick@nickbenn.com>
2020-11-05 17:00:44 -0700

    Verified round trip on user profile request.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerWebService.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerWebService.java
@@ -3,16 +3,21 @@ package edu.cnm.deepdive.codebreaker.service;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import edu.cnm.deepdive.codebreaker.BuildConfig;
+import edu.cnm.deepdive.codebreaker.model.entity.Match;
 import edu.cnm.deepdive.codebreaker.model.entity.User;
 import io.reactivex.Single;
+import java.util.UUID;
 import okhttp3.OkHttpClient;
 import okhttp3.logging.HttpLoggingInterceptor;
 import okhttp3.logging.HttpLoggingInterceptor.Level;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface CodebreakerWebService {
 
