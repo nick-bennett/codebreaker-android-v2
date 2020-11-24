ddea43c21aa3d2988578c78eff4b6db91095673d
Nick Bennett <nick@nickbenn.com>
2020-11-24 05:15:29 -0700

    Add a fragment to navigation.

--- app/src/main/java/edu/cnm/deepdive/codebreaker/controller/NavigationActivity.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/controller/NavigationActivity.java
@@ -39,8 +39,8 @@ public class NavigationActivity extends AppCompatActivity {
   }
 
   private void setupNavigation() {
-    appBarConfig = new Builder(
-        R.id.navigation_game, R.id.navigation_summary, R.id.navigation_settings)
+    appBarConfig = new Builder(R.id.navigation_game, R.id.navigation_summary,
+        R.id.navigation_settings, R.id.navigation_match)
         .setOpenableLayout(binding.drawerLayout)
         .build();
     //noinspection ConstantConditions
--- app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerWebService.java
+++ app/src/main/java/edu/cnm/deepdive/codebreaker/service/CodebreakerWebService.java
@@ -18,6 +18,7 @@ import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public interface CodebreakerWebService {
 
